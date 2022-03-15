c362c85f440020aca2cf032627a1ad180e42ad06
App Academy Student <email@email.com>
2022-03-15 15:10:59 -0400

    finished bands

--- music_app/app/controllers/bands_controller.rb
+++ music_app/app/controllers/bands_controller.rb
@@ -20,6 +20,33 @@ class BandsController < ApplicationController
     end
   end
 
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to bands_url
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find(params[:id])
+
+    if @band.update(band_params) 
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      redirect_to edit_band_url
+    end
+  end
+
   private 
   def band_params
     params.require(:band).permit(:name)
